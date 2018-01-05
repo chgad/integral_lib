f59ee4a6aeb850f3df75215301304c226eb4f12c
chgad <chgad.games@gmail.com>
2018-01-05 21:16:46 +0100

    added rieman right sum

--- riemann.py
+++ riemann.py
@@ -1,4 +1,4 @@
-from .integral import Function, Integral
+from integral import Funktion, Integral
 
 
 class Riemann_left_sum(Integral):
@@ -16,7 +16,7 @@ class Riemann_left_sum(Integral):
 
 
         return space
-
+    
     def integrate(self):
 
         result = 0
@@ -27,4 +27,26 @@ class Riemann_left_sum(Integral):
         
         return result
 
+class Riemann_right_sum(Riemann_left_sum):
 
+     def make_integration_space(self):
+        
+        space = []
+        i = self.lower + self.stepsize
+
+        while ( i <= self.upper ):
+
+            space.append(i)
+            i+= self.stepsize
+
+
+        return space
+
+def x(x):
+    return x
+
+
+result_one = Riemann_left_sum(2.0,0.0,Funktion(x)).integrate()
+result_two = Riemann_right_sum(2.0,0.0, Funktion(x)).integrate()
+
+print(result_one, result_two)
